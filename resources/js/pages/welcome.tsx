d937d97d8b3d224ab2d58f12c48d33ec53fb309e
Mahdi Ansari <mahdi.ansari1@ucalgary.ca>
2025-03-25 12:52:51 -0600

    Fix the font family

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -10,7 +10,7 @@ export default function Welcome() {
                 <link rel="preconnect" href="https://fonts.bunny.net" />
                 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
             </Head>
-            <div className="flex min-h-screen mx-50 flex-col items-center bg-[#FDFDFC] text-[#1b1b18] lg:justify-center dark:bg-[#0a0a0a]">
+            <div className="flex min-h-screen mx-35 flex-col items-center bg-[#FDFDFC] text-[#1b1b18] lg:justify-center dark:bg-[#0a0a0a]">
                 <SiteHeader />
                 
                 <div className="flex w-full items-center justify-center opacity-100 transition-opacity duration-750 lg:grow starting:opacity-0">
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -34,6 +34,7 @@
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,300italic,regular,italic,600,700,700italic" rel="stylesheet">
 
         @routes
         @viteReactRefresh
