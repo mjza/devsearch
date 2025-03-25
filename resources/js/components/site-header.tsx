1c5e08f85191fc039bd8322eb9fbb770270dd179
Mahdi Ansari <mahdi.ansari1@ucalgary.ca>
2025-03-25 11:59:48 -0600

    Change site header to component

---
+++ resources/js/components/site-header.tsx
@@ -0,0 +1,48 @@
+import { type SharedData } from '@/types';
+import { Link, usePage } from '@inertiajs/react';
+
+export default function SiteHeader() {
+    const { auth } = usePage<SharedData>().props;
+    return (
+        <>
+            <header className="w-full bg-white shadow px-15 py-3 flex items-center justify-between dark:bg-secondary">
+                <div className="flex items-center space-x-6">
+                    <nav className="flex items-center space-x-10 text-gray-700">
+                        <a href="/"><h1 className="text-2xl font-extrabold text-blue-600">DevSearch</h1></a>
+                        <a href="#" className="text-base font-medium hover:text-blue-500 dark:text-[#EDEDEC] dark:hover:text-blue-500">Components</a>
+                        <a href="#" className="text-base font-medium hover:text-blue-500 dark:text-[#EDEDEC] dark:hover:text-blue-500">Packages</a>
+                        <a href="#" className="text-base font-medium hover:text-blue-500 dark:text-[#EDEDEC] dark:hover:text-blue-500">Libraries</a>
+                        <a href="#" className="text-base font-medium hover:text-blue-500 dark:text-[#EDEDEC] dark:hover:text-blue-500">News</a>
+                    </nav>
+                </div>
+                <div className="flex items-center space-x-3">
+                    {auth.user ? (
+                        <Link
+                            href={route('dashboard')}
+                            role="button"
+                            className="px-4 py-2 text-sm font-medium rounded-md border border-blue-300 bg-blue-600 text-white hover:bg-blue-700 dark:bg-gray-600 dark:border-gray-600 dark:text-gray-100 dark:hover:bg-gray-700 transition"
+                        >
+                            Dashboard
+                        </Link>
+                    ) : (
+                        <>
+                            <Link
+                                href={route('login')}
+                                className="px-4 py-2 text-sm font-medium rounded-md border border-transparent text-blue-600 hover:bg-blue-50 dark:text-blue-400 dark:hover:bg-blue-900 transition"
+                            >
+                                Log in
+                            </Link>
+                            <Link
+                                href={route('register')}
+                                role="button"
+                                className="px-4 py-2 text-sm font-medium rounded-md border border-blue-600 text-white bg-blue-600 hover:bg-blue-700 dark:border-blue-700 dark:hover:bg-blue-800 transition"
+                            >
+                                Register
+                            </Link>
+                        </>
+                    )}
+                </div>
+            </header>
+        </>
+    );
+}
