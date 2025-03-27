e2f7a22e50210a503cb52d17ffeab3e546578ef5
Mahdi Ansari <mahdi.ansari1@ucalgary.ca>
2025-03-27 13:16:23 -0600

    Add search component

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -1,4 +1,5 @@
 import SiteHeader from '@/components/site-header';
+import SearchBox from '@/components/site-search-box';
 import { Head } from '@inertiajs/react';
 
 
@@ -10,7 +11,7 @@ export default function Welcome() {
                 <link rel="preconnect" href="https://fonts.bunny.net" />
                 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
             </Head>
-            <div className="flex min-h-screen mx-35 flex-col items-center bg-[#FDFDFC] text-[#1b1b18] lg:justify-center dark:bg-[#0a0a0a]">
+            <div className="flex min-h-screen mx-35 flex-col items-center bg-[#F9FAFB] text-[#1b1b18] lg:justify-start dark:bg-[#0a0a0a]">
                 <SiteHeader />
 
                 {/* Title */}
@@ -23,18 +24,11 @@ export default function Welcome() {
                     Search across 10 million package registries and compare features, popularity, and maintenance
                 </div>
 
-
-
-                <div className="flex w-full items-center justify-center opacity-100 transition-opacity duration-750 lg:grow starting:opacity-0">
-
-
-                    <main className="flex w-full max-w-[335px] flex-col-reverse lg:max-w-4xl lg:flex-row">
-                        <div className="flex-1 rounded-br-lg rounded-bl-lg bg-white p-6 pb-12 text-[13px] leading-[20px] shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] lg:rounded-tl-lg lg:rounded-br-none lg:p-20 dark:bg-[#161615] dark:text-[#EDEDEC] dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d]">
-
-
-                        </div>
-                    </main>
-                </div>
+                {/* Search box */}
+                <SearchBox/>
+                 
+                
+                
                 <div className="hidden h-14.5 lg:block"></div>
             </div>
         </>
