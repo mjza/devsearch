e15ac962308ff6f520ec5af6095c5a16a395e303
mahdi.ansari@ucalgary.ca <mahdi.ansari@ucalgary.ca>
2025-03-29 16:07:12 -0600

    Update on seeder

--- database/seeders/NewsItemsTableSeeder.php
+++ database/seeders/NewsItemsTableSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 class NewsItemsTableSeeder extends Seeder
 {
@@ -19,7 +20,7 @@ class NewsItemsTableSeeder extends Seeder
                 'tag' => 'Frontend · React',
                 'title' => '18.0 Released',
                 'description' => 'New features include automatic batching, concurrent rendering, and improved suspense support.',
-                'timestamp' => '2 hours ago',
+                'timestamp' => Carbon::now()->subHours(3),
                 'link' => '#',
                 'is_valid' => true,
             ],
@@ -28,7 +29,7 @@ class NewsItemsTableSeeder extends Seeder
                 'tag' => 'Tooling · Webpack',
                 'title' => 'Webpack 6 Beta',
                 'description' => 'The first beta version of Webpack 6 is out. Includes faster builds and improved tree-shaking.',
-                'timestamp' => '1 day ago',
+                'timestamp' => Carbon::now()->subHours(1),
                 'link' => '#',
                 'is_valid' => true,
             ],
@@ -37,7 +38,7 @@ class NewsItemsTableSeeder extends Seeder
                 'tag' => 'Community',
                 'title' => 'DevSearch Now Open Source',
                 'description' => 'Our core search engine is now open source under the MIT License. Contributions welcome!',
-                'timestamp' => '3 days ago',
+                'timestamp' => Carbon::now()->subHours(2),
                 'link' => '#',
                 'is_valid' => true,
             ],
