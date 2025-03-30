32ae619748edeff311c51e7ad79adfebfe1103bb
mahdi.ansari@ucalgary.ca <mahdi.ansari@ucalgary.ca>
2025-03-29 18:21:48 -0600

    Remove version for moment

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,6 @@
 use App\Http\Controllers\Api\NewsItemController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('api/v1')->group(function () {
+Route::prefix('api')->group(function () {
     Route::get('/news', [NewsItemController::class, 'latest']);
 });
